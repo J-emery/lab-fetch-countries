e98228f54988fc2a05f4d566e578c3b6a8a49f61
J-emery <jamzwj@gmail,com>
2023-03-09 10:22:29 +0000

    made some things work

--- script.js
+++ script.js
@@ -2,20 +2,34 @@ const inputBox = document.querySelector("#country-name-input");
 const mainSection = document.querySelector("#main-section");
 const countryInfo = document.querySelector("#country-info");
 const enterButton = document.querySelector("#enter-button");
+const allCountriesButton = document.querySelector("#all-countries-button");
 
-// const getCountryByName = (countryName) => {
-//     fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-//     .then(response => response.json())
-//     .then(data => countryInfo.innerText = JSON.stringify(data[0]));
-// }
+const getCountryByName = (countryName) => {
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then(response => response.json())
+    .then(data => countryInfo.innerText = data[0].name.common +" has a population of : "+data[0].population);
+ }
 
 const fetchCountryByName = async (countryName) =>{
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
-    const jsonData = await response.json();
-    console.log(typeof jsonData);
-    countryInfo.innerText = JSON.stringify(jsonData);
+    const data = await response.json();
+    countryInfo.innerText = countryInfo.innerText = data[0].name.common +" has a population of : "+data[0].population;
 }
 
 enterButton.addEventListener("click",()=>{
     fetchCountryByName(inputBox.value)
  });
+
+ const fetchAllCountries = async () =>{
+    countryInfo.innerHTML = ""
+    const response = await fetch(`https://restcountries.com/v3.1/all`);
+    const data = await response.json();
+    for (let country of data){
+        countryData = document.createElement("p");
+        countryData.innerText = country.name.common +" has a population of : "+country.population;
+        countryInfo.appendChild(countryData);
+    }
+ }
+allCountriesButton.addEventListener("click",()=>{
+    fetchAllCountries();
+})
